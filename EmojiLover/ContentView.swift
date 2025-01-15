252cfbbf9b0de40c240fb7562dbd4e6809d832dd
anaya-garde <anayasuh@usc.edu>
2025-01-14 22:40:52 -0800

    added emoji project

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 🤩, 👍🏻, 👩🏻‍💻, 🩷
+}
 struct ContentView: View {
+    @State var selection: Emoji = .👩🏻‍💻
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+               
+                Picker("Select Emoji",  selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
